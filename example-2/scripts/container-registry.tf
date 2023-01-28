d9fe6844b528d4ea5e84bb293fbcc8d930bc5797
Pakapop Cheunchomsirakul <abounk@Pakapops-MacBook-Pro.local>
2023-01-28 20:40:56 +0700

    create container registry in example2

--- example-2/scripts/container-registry.tf
+++ example-2/scripts/container-registry.tf
@@ -1,7 +1,7 @@
 # Creates a container registry on Azure so that you can publish your Docker images.
 
 resource "azurerm_container_registry" "container_registry" {
-  name                = "flixtube"
+  name                = "flixtube6031"
   resource_group_name = azurerm_resource_group.flixtube.name
   location            = "westus"
   admin_enabled       = true
@@ -10,12 +10,15 @@ resource "azurerm_container_registry" "container_registry" {
 
 output "registry_hostname" {
   value = azurerm_container_registry.container_registry.login_server
+  sensitive = true
 }
 
 output "registry_un" {
   value = azurerm_container_registry.container_registry.admin_username
+  sensitive = true
 }
 
 output "registry_pw" {
   value = azurerm_container_registry.container_registry.admin_password
+  sensitive = true
 }
